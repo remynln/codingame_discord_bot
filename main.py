86d7f3818ef1ea9a98fee87c3d8dcd6c7dd380c8
Waz0x <remy.noulin@epitech.eu>
2021-07-19 16:28:18 +0200

    MOD | infos command

--- main.py
+++ main.py
@@ -106,6 +106,15 @@ async def next(ctx):
     embed.set_footer(text="Time before start: " + str(coc.time_before_start))
     await ctx.send(embed=embed)
 
+@bot.command(name="info", description="Display info on the bot")
+async def info(ctx):
+    embed = discord.Embed(title="Infos", color=Color.gold())
+    embed.add_field(name="Invite me", value="[here](https://discord.com/api/oauth2/authorize?client_id=866601410237038592&permissions=8&scope=bot)")
+    embed.add_field(name="Source Code", value="[here](https://github.com/Waz0x/codingame_discord_bot)")
+    embed.set_thumbnail(url="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/v1410916443/e1aka8oyy6vnsbrt8ogw.png")
+    embed.set_footer(text="Made with <3 by Waz0x", icon_url="https://cdn.discordapp.com/avatars/606758395583922176/0ab96a13c0e7998926b1ffdfa9364313.png?size=128")
+    await ctx.send(embed=embed)
+
 #@bot.command(name="leaderboard", description="get your leaderboard")
 #async def leaderboard(ctx, arg=None):
 #    if not arg:
