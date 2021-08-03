ff925dff3f4db2a6183532feaa8ea79e79c3a988
Saverio <personnex976@gamil.com>
2021-08-03 11:34:42 +0200

    all on_* function

---
+++ cogs/on_event.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class OnEvent(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        await ctx.send(f"An error occured: {str(error)}")
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('bot is ready')
+        activity = discord.Game(name=self.bot.config["prefix"] + "help")
+        await self.bot.change_presence(activity=activity)
+
+def setup(bot: commands.Bot):
+    bot.add_cog(OnEvent(bot))
